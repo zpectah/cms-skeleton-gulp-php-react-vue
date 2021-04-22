daae94d85c7566041195814c7bd3354d0c98deb8
Tomas Sychra <sychrat@gmail.com>
2021-04-22 21:00:40 +0200

    remove unused imports

--- src/admin/scripts/App/page/Dashboard.tsx
+++ src/admin/scripts/App/page/Dashboard.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { useTranslation } from 'react-i18next';
 
 import routes from '../routes.json';
--- src/admin/scripts/App/page/Error404.tsx
+++ src/admin/scripts/App/page/Error404.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { useTranslation } from 'react-i18next';
 import styled from 'styled-components';
 
